9f6531dbb548d340a0c5a9141a62eebcc4a82f61
escobartc <carlosescobartriana@gmail.com>
2024-03-07 10:23:48 -0500

    feature-19: Remove logger printer

--- tte-app/src/main/java/com/challenge/tteapp/service/LegacyAdmin.java
+++ tte-app/src/main/java/com/challenge/tteapp/service/LegacyAdmin.java
@@ -1,9 +1,8 @@
 package com.challenge.tteapp.service;
 
-import com.challenge.tteapp.LoggerPrinter;
 import com.challenge.tteapp.model.Product;
 import org.springframework.http.ResponseEntity;
 
 public interface LegacyAdmin  {
-    ResponseEntity<Object> saveProduct(Product product, LoggerPrinter loggerPrinter);
+    ResponseEntity<Object> saveProduct(Product product, String requestId);
 }
--- tte-app/src/main/java/com/challenge/tteapp/service/impl/AdminServiceImpl.java
+++ tte-app/src/main/java/com/challenge/tteapp/service/impl/AdminServiceImpl.java
@@ -1,30 +1,30 @@
 package com.challenge.tteapp.service.impl;
 
-import com.challenge.tteapp.LoggerPrinter;
 import com.challenge.tteapp.model.Product;
 import com.challenge.tteapp.processor.ValidationError;
 import com.challenge.tteapp.repository.AdminRepository;
 import com.challenge.tteapp.repository.ProductRepository;
 import com.challenge.tteapp.service.LegacyAdmin;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.logging.LogLevel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Service
+@Slf4j
 public class AdminServiceImpl implements LegacyAdmin {
 
     final AdminRepository adminRepository;
     final ProductRepository productRepository;
     final ValidationError validationError;
 
-    public ResponseEntity<Object> saveProduct(Product product, LoggerPrinter loggerPrinter) {
-        loggerPrinter.log(LogLevel.INFO, "Save Product in Database");
+    public ResponseEntity<Object> saveProduct(Product product, String requestId) {
+        log.info("Save Product in Database {}", requestId);
         if (product.getTitle() == null || product.getTitle().isEmpty()) {
-            loggerPrinter.log(LogLevel.ERROR, "Product title cannot be empty");
+            log.error("Product title cannot be empty {}", requestId);
             return new ResponseEntity<>(validationError.getStructureError(HttpStatus.BAD_REQUEST.value(),
                     "Product title cannot be empty"), HttpStatus.BAD_REQUEST);
         }
