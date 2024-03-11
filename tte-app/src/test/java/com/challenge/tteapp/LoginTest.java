a641dc85e93abeda7965971cc67ffdc1df8318fa
Erik Andrey Lamprea Rivero <Erik-Andrey.Lamprea@endava.com>
2024-03-11 07:26:52 -0500

    correction package in unit test

--- tte-app/src/test/java/com/challenge/tteapp/LoginTest.java
+++ tte-app/src/test/java/com/challenge/tteapp/LoginTest.java
@@ -1,25 +1,6 @@
 package com.challenge.tteapp;
 
-import com.challenge.tteapp.model.dto.UserDTO;
-import com.challenge.tteapp.processor.JwtService;
-import com.challenge.tteapp.processor.ValidationError;
-import com.challenge.tteapp.processor.ValidationResponse;
-import com.challenge.tteapp.repository.ProductRepository;
-import com.challenge.tteapp.repository.UserRepository;
-import com.challenge.tteapp.service.AdminService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
 
 
 public class LoginTest {
