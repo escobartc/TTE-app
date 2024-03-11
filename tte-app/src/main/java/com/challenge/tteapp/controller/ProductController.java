f2c87902d10b3c176f7a9efdf01d26eaa3d316e6
escobartc <carlosescobartriana@gmail.com>
2024-03-11 14:02:32 -0500

    feature-20: Updated Product Creation logic with token and roles

--- tte-app/src/main/java/com/challenge/tteapp/controller/ProductController.java
+++ tte-app/src/main/java/com/challenge/tteapp/controller/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -26,11 +28,37 @@ public class ProductController {
         List<ProductDTO> products = productService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
     @PostMapping(path = "/product", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> createProduct(@RequestBody ProductDTO product) {
-        String requestId = UUID.randomUUID().toString();
-        log.info("JOIN TO TTE-APP with requestId: {}", requestId);
-        ResponseEntity<Object> response = productService.saveProduct(product, requestId);
+    public ResponseEntity<Object> createProduct(@RequestBody ProductDTO product, Authentication authentication) {
+        try {
+            String requestId = UUID.randomUUID().toString();
+            log.info("JOIN TO TTE-APP with requestId: {}", requestId);
+            verifyAuthorization(product, authentication);
+            // Save the product
+            ResponseEntity<Object> response = productService.saveProduct(product, requestId);
+            Map<String, Object> responseBody = getStringObjectMap(response);
+            return ResponseEntity.status(response.getStatusCode()).body(responseBody);
+        } catch (Exception e) {
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("message", "Failed to create product");
+            log.error("Error creating product: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        }
+    }
+
+    private static void verifyAuthorization(ProductDTO product, Authentication authentication) {
+        // Get the authenticated user's authorities (roles)
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        // Check if the user has the "employee" role
+        boolean isEmployee = authorities.stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_employee"));
+
+        product.setState((isEmployee) ? "Pending" : "Approved");
+    }
+
+    private static Map<String, Object> getStringObjectMap(ResponseEntity<Object> response) {
         Long productId = null;
         if (response.getStatusCode() == HttpStatus.CREATED) {
             productId = ((Product) Objects.requireNonNull(response.getBody())).getId();
@@ -42,7 +70,7 @@ public class ProductController {
         } else {
             responseBody.put("message", "Failed to create product");
         }
-        return ResponseEntity.status(response.getStatusCode()).body(responseBody);
+        return responseBody;
     }
 
 
--- tte-app/src/main/java/com/challenge/tteapp/repository/ProductRepository.java
+++ tte-app/src/main/java/com/challenge/tteapp/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.challenge.tteapp.repository;
 import com.challenge.tteapp.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByInventoryAvailableGreaterThan(int available);
 }
