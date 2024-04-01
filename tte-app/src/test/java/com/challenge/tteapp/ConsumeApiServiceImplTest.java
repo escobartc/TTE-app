74ff969b86420b12ac787aaef42aa8a2991a1014
Erik Andrey Lamprea Rivero <Erik-Andrey.Lamprea@endava.com>
2024-04-01 17:34:51 -0500

    correction code smells

--- tte-app/src/test/java/com/challenge/tteapp/ConsumeApiServiceImplTest.java
+++ tte-app/src/test/java/com/challenge/tteapp/ConsumeApiServiceImplTest.java
@@ -21,7 +21,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ConsumeApiServiceImplTest {
+class ConsumeApiServiceImplTest {
 
 
     @Mock
