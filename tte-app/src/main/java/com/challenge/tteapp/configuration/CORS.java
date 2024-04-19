0eed156dc146355619d0582bfd5f2d415e3a6d1e
LAMPREA RIVERO ERIK ANDREY <ealamprear@udistrital.edu.co>
2024-04-19 13:56:52 -0500

    add cors

---
+++ tte-app/src/main/java/com/challenge/tteapp/configuration/CORS.java
@@ -0,0 +1,18 @@
+package com.challenge.tteapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORS implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
